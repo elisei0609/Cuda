321c4803fddc0d38a945bd666c55fcc9c92c186f
Yelysei <elisei2015@gmail.com>
2019-09-27 11:49:03 +0300

    x mark rotate in header by click

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-
-function toggleBurger (){
-   const burger = document.getElementById('burger');
-   const nav = document.getElementById('nav');
-   burger.addEventListener('click', (e) => {
-      nav.classList.toggle('nav--active')
-   })
-   
-};
+function toggleBurger() {
+  const burger = document.getElementById("burger");
+  const nav = document.getElementById("nav");
+  burger.addEventListener("click", e => {
+    nav.classList.toggle("nav--active");
+  });
+}
 toggleBurger();
+
+// Rotating  X Mark on burger menu
+
+function toggleXBurger() {
+  const burger = document.getElementById("burger");
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("burger--active");
+  });
+}
+toggleXBurger();
